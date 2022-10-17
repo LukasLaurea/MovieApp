6a5d6bcd8e6bc97c2bc8814e561e1c0703c066d3
Lukas Ritter <2206923@laurea.fi>
2022-10-17 21:26:07 +0300

    initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'c53c94eeba47c66bd93aa07010378704',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
